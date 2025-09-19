b8cf01cd301cafc8ec1ba8506c91b6303e9e03db
William Henning <wchenningwchenning@gmail.com>
2025-09-19 01:37:17 -0400

    Delete unused generic backend base class

--- backend.h
+++
@@ -1,9 +0,0 @@
-#include <memory>
-
-#include "third_party/status/status_or.h"
-
-class Backend {
- public:
-  virtual int port() = 0;
-  virtual ~Backend() = default;
-};
--- wayland_backend.h
+++ wayland_backend.h
@@ -5,18 +5,17 @@
 #include <string>
 #include <vector>
 
-#include "backend.h"
 #include "third_party/status/status_or.h"
 
-class WaylandBackend : public Backend {
+class WaylandBackend {
  public:
   static StatusOr<std::unique_ptr<WaylandBackend>> start_server(
       int32_t port_offset, int32_t width, int32_t height,
       const std::vector<std::string>& command);
 
-  ~WaylandBackend() override;
+  ~WaylandBackend();
 
-  int port() override { return port_; }
+  int port() { return port_; }
 
  private:
   WaylandBackend(int port, int weston_pid, int subproc_pid)
